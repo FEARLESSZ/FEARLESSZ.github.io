faa6ccb53bd22fe3511058cb6918b374faecef27
Zane Zollers <zfearlessz09@gmail.com>
2021-10-28 16:14:48 -0500

    functional_search

---
+++ StringFormat.php
@@ -0,0 +1,12 @@
+$search = $_POST['searchbar'];
+$prereq = "https://www.google.com/search?q=";
+$final = $prereq;
+for($i = (-1 * strlen($search)); i < 0; i++) {
+    if(substr($search, i, 1) == " ") {
+        $final .= +;
+    }
+    else {
+        $final .= substr($search, i, 1);
+    }
+}
+header('Location:' + $final);
